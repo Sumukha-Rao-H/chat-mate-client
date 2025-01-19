92895fdea965d3883e37eaf0fae401eabfbb3dd7
Sumukha Rao H <sumukharaoh05@gmail.com>
2025-01-19 15:49:08 +0530

    bug fix 2

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000";
+
+const SOCKET_URL = `${process.env.SERVER_URL}`;
 const socket = io(SOCKET_URL);
 
 export default socket;
