6ed096b83b5f7dc45c51771c2ba416b5fa4b5d22
Sumukha Rao H <sumukharaoh05@gmail.com>
2025-02-07 09:43:34 +0530

    removed namespacing to make chat working

--- src/sockets/chatSocket.js
+++ src/sockets/chatSocket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = `${process.env.REACT_APP_SERVER_URL}/chatSocket`;
+const SOCKET_URL = `${process.env.REACT_APP_SERVER_URL}`;
 const chatSocket = io(SOCKET_URL);
 
 export default chatSocket;
